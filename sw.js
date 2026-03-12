29403877c13b23ea9be8f502e08e6783d360336b
aros111 <121636214+aros111@users.noreply.github.com>
2026-03-12 21:34:38 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BelegScan Pro – Service Worker v8
-const V = 'bsp-v11';
+const V = 'bsp-v12';
 const ASSETS = [
   './', './index.html',
   'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400&family=DM+Mono:wght@300&display=swap',
